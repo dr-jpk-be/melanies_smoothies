6362f4ab57a5b8d4d85a0ae3361a7eab673639dc
dr-jpk-be <163514123+dr-jpk-be@users.noreply.github.com>
2024-07-31 00:05:43 +0200

    Update streamlit_app.py
    final final

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 )
 
 name_on_order = st.text_input("Name on Smoothie:")
-st.write("The name on your Smoothie will be:", name_on_order)
+# st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -36,7 +36,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     # st.write(ingredients_string)
